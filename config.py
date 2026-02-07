ca0a85012a6d61da4d919bb75396114f904a4adf
Peter Paprotny <pappet@web.de>
2026-02-08 00:17:11 +0100

    feat(02-01): define sprite layers - Create a new Enum called SpriteLayer. - This enum will define the fixed rendering order for sprite layers on a tile.

--- config.py
+++ config.py
@@ -1,4 +1,18 @@
+from enum import Enum
+
+
 # Game configuration
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Rogue Like RPG"
+
+
+class SpriteLayer(Enum):
+    GROUND = 0
+    DECOR_BOTTOM = 1
+    TRAPS = 2
+    ITEMS = 3
+    CORPSES = 4
+    ENTITIES = 5
+    DECOR_TOP = 6
+    EFFECTS = 7
