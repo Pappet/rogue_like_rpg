e262794d492996a4978b48f7310afa74be12dfdf
Peter Paprotny <pappet@web.de>
2026-02-08 21:34:30 +0100

    chore(03-01): remove obsolete entity and service files

--- entities/hero.py
+++
@@ -1,8 +0,0 @@
-class Hero:
-    def __init__(self, name: str, hp: int, max_hp: int):
-        self.name = name
-        self.hp = hp
-        self.max_hp = max_hp
-
-    def __repr__(self):
-        return f"Hero(name={self.name}, hp={self.hp}/{self.max_hp})"
